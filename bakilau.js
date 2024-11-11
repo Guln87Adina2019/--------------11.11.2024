dbc3a2732e82b59330e25d565d36b612ff9c554e
Gulnara <adinadzulia@gmail.com>
2024-11-11 10:54:46 +0500

    add bak

---
+++ bakilau.js
@@ -0,0 +1,44 @@
+// Тапсырма А: Ай атауын шығару
+
+// let  san=+prompt ("1-ден 12-ге дейінгі сан енгізіңіз:")
+
+// if (san == 3|| san== 4 ||san==5) {
+    
+//     alert("Spring" )
+// }
+// if (san == 6|| san==7|| san==8){
+//     alert("Summer")
+// }
+   
+
+// if (san ==9|| san==10|| san==11){
+    
+//     alert("Autumn")
+// }
+// if (san == 12|| san==1|| san==2) {
+    
+//     alert("Winter")
+// }
+
+// else
+//  ("0-ден төмен;12-ден жоғары болса:")
+//      {alert("Ондай сан жоқ.Қайта таңдауыңызды өтінем!")}
+
+
+//      Тапсырма B:Екі санды салыстырып,орташа мәнді табу
+let numA =+prompt("A Санын енгізіңіз")
+let numB =+prompt("B Санын енгізіңіз")
+
+// if (a>b || a<b){
+//     numA=a>b
+//     alert("a саны b санынан үлкен"+numA)
+//     else{ numA=a<b
+//         alert("a саны b санынан үлкен"+numB)
+
+//     }
+// }
+switch(a>b){
+    case numA=(a>b)
+    alert("a саны b санынан үлкен"+ numA)
+    break
+}

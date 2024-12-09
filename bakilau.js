1a1ef76621942fc6cd284d20a756092cafbe21dc
Gulnara <adinadzulia@gmail.com>
2024-12-09 10:57:05 +0500

    add bak

--- bakilau.js
+++ bakilau.js
@@ -26,8 +26,8 @@
 
 
 //      Тапсырма B:Екі санды салыстырып,орташа мәнді табу
-let numA =+prompt("A Санын енгізіңіз")
-let numB =+prompt("B Санын енгізіңіз")
+// let numA =+prompt("A Санын енгізіңіз")
+// let numB =+prompt("B Санын енгізіңіз")
 
 // if (a>b || a<b){
 //     numA=a>b
@@ -37,8 +37,76 @@ let numB =+prompt("B Санын енгізіңіз")
 
 //     }
 // }
-switch(a>b){
-    case numA=(a>b)
-    alert("a саны b санынан үлкен"+ numA)
-    break
-}
+// switch(a>b){
+//     case numA=(a>b)
+//     alert("a саны b санынан үлкен"+ numA)
+// }
+
+//task 1
+
+/* let season = +prompt("1-ден 12-ге дейінгі санды енгізіңіз: ")
+let san = 0
+switch (season) {
+    case 12  1  2:
+        alert("Ай: " + season + "Маусым: Winter")
+        break;
+    case 3  4  5:
+        alert("Ай: " + season + "Маусым: Spring")
+        break;
+
+    case 6  7  8:
+        alert("Ай: " + season + "Маусым: Summer")
+        break;
+    case 9 10  11:
+        alert("Ай: " + season + "Маусым: Autumn")
+        break;
+
+         default:
+            alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+    
+}   */
+ 
+// Task 2
+ 
+/* let a = +prompt("a санын енгізіңіз: ")
+let b = +prompt("b санын енгізіңіз: ")
+
+if (a<b) {
+   alert("a саны b санынан кіші ")}
+
+else if (a>b){
+    alert( "a саны b санынан үлкен ")
+}   
+ else {
+    alert("a саны b санына тең")    
+} 
+alert("a және b сандарының орташа мәні: " + (a+b)/2 ) */
+
+// Task 3
+
+let num1 = +prompt("Бірінші санды енгізіңіз: ")
+let num2 = +prompt("Екінші санды енгізіңіз: ")
+let num3 = +prompt("Үшінші санды енгізіңіз: ")
+
+let max;
+let min;
+
+ if(num1>=num2 && num1>=num3){
+    max=num1
+ }else if(num2>=num1 && num2>=num3){
+    max=num2
+ }
+ else if(num3>=num1 && num3>=num2){
+    max=num3
+ }
+ if(num1<=num2 && num1<=num3){
+    min=num1
+ }else if(num2<=num1 && num2<=num3){
+    min=num2
+ }
+ else if(num3<=num1 && num3<=num2){
+    min=num3
+ }
+
+ 
+ alert("Ең үлкен және ең кіші сандың айырмасы: " +(max-min) )
---
+++ Аралық бақылау6/E-ShopObject.js
@@ -0,0 +1,126 @@
+// let eShop = {
+//     name:"Ays",
+//     phoneNomber:"87778311985",
+//     ListproductNames:[
+//         {name:"Arbuz",category:"Baksha",price:"10000",stock:"500"},
+//         {name:"Sleeppi",category:"Kyim",price:"10000",stock:"100"},
+//         {name:"Nuts",category:"Tatti",price:"1000",stock:"200"},
+//         {name:"Sumsung",category:"Elektronica",price:"1000000",stock:"300"},
+//         {name:"Okno",category:"Stroi",price:"300000",stock:"50"},
+//     ]
+// }
+// console.log(eShop);
+
+
+
+// function changeShopname ="Ays";
+// let newName = +prompt ("Жаңа атау енгіз")
+
+// console.log(changeShopname);
+
+
+
+
+let phoneNumber = "87778311985";  
+
+function changePhoneNumber(87778311985) {
+  let newNumber = +prompt("Жаңа телефон нөміріңізді енгізіңіз (қалдыру үшін Enter басыңыз):");
+
+  if (newNumber.() !== "") {
+    phoneNumber = newNumber;
+  }
+
+  console.log("Қазіргі телефон нөмірі: " + phoneNumber);
+}
+
+
+changePhoneNumber(87778311985);
+
+
+
+
+function listProductNames() {
+    
+    const products = ["Өнім 1", "Өнім 2", "Өнім 3", "Өнім 4","Өнім 5"];
+    
+    
+    alert(products.(""));
+  }
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
